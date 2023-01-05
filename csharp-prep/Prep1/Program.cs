65a59d2e9fa1148599cc44830b0e1ed67d387854
ohheydenise <wil19015@byui.edu>
2023-01-05 15:34:03 -0700

    Wrote code to get user's first and last name, then display it

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Hello Prep 1 World!");
+
+        Console.Write("What is your first name? ");
+        string zw_first = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string zw_last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {zw_last}, {zw_first} {zw_last}.");
     }
 }
