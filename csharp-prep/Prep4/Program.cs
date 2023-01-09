3f07e7ad6d55d67877edb9a972155041a17b7622
ohheydenise <wil19015@byui.edu>
2023-01-09 14:05:22 -0700

    Created list program

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> zw_nums = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        float zw_sum = 0;
+
+        while (zw_nums.IndexOf(0) == -1) {
+            Console.Write("Please enter a number: ");
+            int zw_num_user = Convert.ToInt32(Console.ReadLine());
+            zw_nums.Add(zw_num_user);
+            zw_sum += zw_num_user;
+        }
+
+        float zw_length = zw_nums.Count() - 1;
+        float zw_average = zw_sum / zw_length;
+        int zw_highest = zw_nums.Max();
+        Console.WriteLine($"The sum is: {zw_sum}");
+        Console.WriteLine($"The average is: {zw_average}");
+        Console.WriteLine($"The largest number is: {zw_highest}");
     }
 }
